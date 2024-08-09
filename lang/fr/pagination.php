7af7676cde5b6a077b80ba06b9b372b8db9ad945
Lamine Diallo <bah07565@gmail.com>
2024-08-09 10:49:36 +0000

    Publication des langues et traduction en français

---
+++ lang/fr/pagination.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Lignes de Langue pour la Pagination
+    |--------------------------------------------------------------------------
+    |
+    | Les lignes de langue suivantes sont utilisées par la bibliothèque de pagination
+    | pour créer les liens de pagination simples. Vous êtes libre de les modifier
+    | comme bon vous semble pour personnaliser vos vues et mieux adapter votre application.
+    |
+    */
+
+    'previous' => '&laquo; Précédent',
+    'next' => 'Suivant &raquo;',
+];
---
+++ lang/fr/passwords.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Lignes de Langue pour la Réinitialisation du Mot de Passe
+    |--------------------------------------------------------------------------
+    |
+    | Les lignes de langue suivantes sont les messages par défaut correspondant
+    | aux raisons pour lesquelles une tentative de mise à jour de mot de passe
+    | a échoué, comme un mot de passe invalide ou un jeton de réinitialisation
+    | non valide.
+    |
+    */
+
+    'reset' => 'Votre mot de passe a été réinitialisé.',
+    'sent' => 'Nous vous avons envoyé par e-mail le lien de réinitialisation du mot de passe.',
+    'throttled' => 'Veuillez patienter avant de réessayer.',
+    'token' => 'Ce jeton de réinitialisation de mot de passe est invalide.',
+    'user' => "Nous ne trouvons pas d'utilisateur avec cette adresse e-mail.",
+
+];
