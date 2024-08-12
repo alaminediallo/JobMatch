8b3774fb332bb416248744066a9c1c8513275bd3
Lamine Diallo <bah07565@gmail.com>
2024-08-12 14:46:44 +0000

    Ajout des informations supplémentaires pour le "user" (type_user)

---
+++ database/migrations/2024_08_12_113929_add_type_user_to_user_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->after('name', function () use ($table) {
+                $table->string('prenom')->nullable();
+            });
+
+            $table->after('password', function () use ($table) {
+                $table->enum('type_user', ['Candidat', 'Administrateur', 'Entreprise']);
+                $table->string('adresse')->nullable();
+                $table->string('nom_entreprise')->nullable();  // Specific to Entreprise
+                $table->text('description_entreprise')->nullable();  // Specific to Entreprise
+                $table->string('localisation')->nullable();  // Specific to Entreprise
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('user', function (Blueprint $table) {
+            //
+        });
+    }
+};
