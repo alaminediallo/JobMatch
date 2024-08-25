1cc165a21c0281c71fcbe18bcc285ed977536e14
Lamine Diallo <bah07565@gmail.com>
2024-08-25 14:22:04 +0000

    Ajout du module "categories" pour les offres d'emploi

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
             PermissionSeeder::class,
             RoleSeeder::class,
             TypeEntrepriseSeeder::class,
+            CategorySeeder::class,
         ]);
 
         $roles = Role::whereIn('name', ['Administrateur', 'Recruteur', 'Candidat'])
